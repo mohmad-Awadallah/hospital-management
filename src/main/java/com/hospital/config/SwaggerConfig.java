7800b2432a6d4087da00a988bc2a4b94c500081f
mohmad-Awadallah <134318508+mohmad-Awadallah@users.noreply.github.com>
2025-05-23 22:20:15 +0300

    01

--- src/main/java/com/hospital/config/SwaggerConfig.java
+++ src/main/java/com/hospital/config/SwaggerConfig.java
@@ -1,38 +1,13 @@
 package com.hospital.config;
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+
 
 @Profile("dev") // يعمل فقط في بيئة التطوير
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.hospital.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Hospital Management API")
-                .description("API for managing hospital operations")
-                .version("1.0")
-                .contact(new Contact("Hospital Team", "https://hospital.com", "info@hospital.com"))
-                .license("Apache 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
-                .build();
-    }
 }
